afbd3076cf8f6a3ac85de1211ec6a4e545bc12c2
taijulsir <princetaijul123@gmail.com>
2023-12-21 22:36:32 +0600

    Connect with mongodb

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.49cfwvw.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+  
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/',async(req,res)=>{
     res.send("The task management server is running")
 })
