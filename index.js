cd170d76d0c37a5327c461f750d309a283cf7779
taijulsir <princetaijul123@gmail.com>
2023-12-22 19:27:13 +0600

    added update and delete method

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.49cfwvw.mongodb.net/?retryWrites=true&w=majority`;
 
 
@@ -27,17 +27,51 @@ async function run() {
 
     const taskCollection = client.db('taskManagementDB').collection('createTasks')
 
-    app.post('/createTask',async(req,res)=>{
-        const task = req.body;
-        const result = await taskCollection.insertOne(task)
-        res.send(result)       
+    app.post('/createTask', async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task)
+      res.send(result)
     })
-    app.get('/createTast/:email',async(req,res)=>{
-        const email = req.params.email;
-        const query = {email: email}
-        const result = await taskCollection.find(query).toArray()
-        res.send(result)
+    app.get('/toDoList/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const result = await taskCollection.find(query).toArray()
+      res.send(result)
     })
+    app.get('/updatetask/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      console.log(query)
+      const result = await taskCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.delete("/taskDelete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      console.log(query)
+      const result = await taskCollection.deleteOne(query)
+      console.log(result)
+      res.send(result)
+    })
+
+    app.patch("/toDoList/:id", async (req, res) => {
+      const data = req.body
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const updatedStatus = {
+        $set: {
+          email: data.email,
+          priority: data.priority,
+          title: data.title,
+          description: data.description,
+          deadline: data.deadline,
+        }
+      }
+      const result = await taskCollection.updateOne(query, updatedStatus)
+      res.send(result)
+    })
+
 
 
     // await client.db("admin").command({ ping: 1 });
@@ -50,9 +84,9 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',async(req,res)=>{
-    res.send("The task management server is running")
+app.get('/', async (req, res) => {
+  res.send("The task management server is running")
 })
-app.listen(port,()=>{
-    console.log(`The server is running on ${port} port`)
+app.listen(port, () => {
+  console.log(`The server is running on ${port} port`)
 })
